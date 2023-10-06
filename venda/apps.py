ebbccb68553aa585e9885169817eb5055ff474be
Norrbs <mateusnorberto7@gmail.com>
2023-10-06 19:18:21 -0300

    mudar aplicativo para venda

--- venda/apps.py
+++ venda/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AluguelConfig(AppConfig):
+class VendaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aluguel'
+    name = 'venda'
